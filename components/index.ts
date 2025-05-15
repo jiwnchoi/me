d3cebcdb7fa1e666d31a69073e69ae6a0cdafd64
jiwnchoi <jiwnchoi@skku.edu>
2025-05-15 22:17:39 +0900

    fix: remove GoTop export from index.ts

--- components/index.ts
+++ components/index.ts
@@ -1,6 +1,5 @@
 export { default as Date } from "./Date";
 export { default as Giscus } from "./Giscus";
-export { default as GoTop } from "./GoTop";
 export { default as MDXContent } from "./MDXContent";
 export { default as Navigation } from "./Navigation";
 export { default as Responsive } from "./Responsive";
